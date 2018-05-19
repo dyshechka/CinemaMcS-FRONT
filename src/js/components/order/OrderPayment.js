d1e7039c5cda395ed35461da9e80ca3671cd16e2
garro <ezenhorn@mail.ru>
2018-05-19 10:56:14 +0300

    before adding card mask

--- src/js/components/order/OrderPayment.js
+++ src/js/components/order/OrderPayment.js
@@ -3,6 +3,7 @@ import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import {addSeatToOrder, calculateOrder} from "../../actions/order_actions";
 import OrderFilmPlate from "./OrderFilmPlate";
+import {Container, Input, Row} from "reactstrap";
 
 class OrderPayment extends Component {
 
@@ -12,11 +13,35 @@ class OrderPayment extends Component {
         this.props.calculateOrder({seanceId: this.props.seance.id, seatIds: seatsIds});
     }
 
+    getMonths = () => {
+        let months = [];
+        for (let i = 1; i < 13; i++) {
+           months.push((<option key={"month-"}>{i}</option>))
+        }
+        return months;
+    };
+
     render() {
         return (
             <div>
                 <OrderFilmPlate/>
-                Order payment
+                <Container>
+                    <Row className="justify-content-center">
+                        <Input className="large-input-field text-uppercase" placeholder="НОМЕР КАРТЫ"/>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Input className="large-input-field text-uppercase" placeholder="ИМЯ И ФАМИЛИЯ ВЛАДЕЛЬЦА КАРТЫ"/>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Input type="select"className="small-input-field" placeholder="ММ">
+                            {
+                                this.getMonths()
+                            }
+                        </Input>
+                        <Input className="small-input-field" placeholder="ГГГГ"/>
+                        <Input className="small-input-field" placeholder="CVV"/>
+                    </Row>
+                </Container>
             </div>
         );
     }
